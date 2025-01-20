f95428b13d8e629e90a2832e298760be6693d3ab
joaoleandro <new.joaoleandro@gmail.com>
2025-01-20 13:48:57 -0300

    feat: buscar usuario pelo id

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+}
