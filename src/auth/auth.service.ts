20b17c067028bf18c4a4ea074de3e43094873917
joaoleandro <new.joaoleandro@gmail.com>
2025-01-20 21:07:28 -0300

    env example and jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { UserService } from './../user/user.service';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  @Inject()
+  private readonly userService: UserService;
+
+  @Inject()
+  private readonly jwtService: JwtService;
+
+  async signin(
+    params: Prisma.UserCreateInput,
+  ): Promise<{ access_token: string }> {
+    const user = await this.userService.user({ email: params.email });
+    if (!user) throw new NotFoundException('User not found');
+
+    const passwordMatch = await bcrypt.compare(params.password, user.password);
+    if (!passwordMatch) throw new NotFoundException('Invalid credials.');
+
+    const payload = { sub: user.id };
+
+    return { access_token: await this.jwtService.signAsync(payload) };
+  }
+}
